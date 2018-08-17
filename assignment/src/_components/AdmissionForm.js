335abdfaf4d677ee001d13aab522253a3817d0b3
Sameer <sameersitre@gmail.com>
2018-08-17 17:41:23 +0530

    added user validation and currying to avoid rerendering

--- assignment/src/_components/AdmissionForm.js
+++ assignment/src/_components/AdmissionForm.js
@@ -12,28 +12,28 @@ class AdmissionForm extends Component {
         }
     }
 
-    handleOnChange(event) {
+    handleOnChange=(param)=>(event) =>{
         this.setState({ name: event.target.value });
-    }
-
-    formValidator(){
-
+        
 
     }
+
+    
+//different callback instance is created each time the component is rendered so to avoid it, currying is used.
     render() {
         return (
             <div className="newusercontainer">
                 <h2 className='heading'>Student Admission Form</h2>
                 <table>
-                    <tr> <td>Student Name:</td> <td><input id='input' type='text' placeholder="Name" value={this.state.anything} onChange={this.handleOnChange.bind(this)} /></td></tr>
+                    <tr> <td>Student Name:</td> <td><input id='input' type='text' maxLength='20' placeholder="Name" value={this.state.anything} onChange={this.handleOnChange('name')} /></td></tr>
                     <br /> 
-                    <tr> <td>Last Name:</td><td><input id='input'  placeholder="Sur Name" value={this.state.anything} onChange={this.handleOnChange.bind(this)} /></td></tr>
+                    <tr> <td>Last Name:</td><td><input id='input' maxLength='20' placeholder="Sur Name" value={this.state.anything} onChange={this.handleOnChange('lastname')} /></td></tr>
                     <br /> 
-                    <tr> <td>Class:</td><td><input id='input' placeholder="Class" value={this.state.anything} onChange={this.handleOnChange.bind(this)} /></td></tr>
+                    <tr> <td>Class:</td><td><input id='input' maxLength='4' placeholder="Class" value={this.state.anything} onChange={this.handleOnChange('class')} /></td></tr>
                     <br /> 
-                    <tr> <td>Year of Passing:</td><td><input id='input' placeholder="Passing Year" value={this.state.anything} onChange={this.handleOnChange.bind(this)} /></td></tr>
+                    <tr> <td>Year of Passing:</td><td><input id='input' placeholder="Passing Year" value={this.state.anything} onChange={this.handleOnChange('year')} /></td></tr>
                     <br /> 
-                    <tr> <td>Percentages:</td><td><input id='input' type='number' placeholder="Percentage of Marks" value={this.state.anything} onChange={this.handleOnChange.bind(this)} /></td></tr>
+                    <tr> <td>Percentages:</td><td><input id='input' type='number' placeholder="Percentage of Marks" value={this.state.anything} onChange={this.handleOnChange('percent')} /></td></tr>
                 </table>
                 <br/>
                <h4>{this.state.suggestions}</h4> 
